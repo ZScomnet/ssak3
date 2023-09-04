5d8a3bd52abb58005ad7f5feb567c8c765fdefe8
김예진 <kky156@naver.com>
2023-09-04 15:07:39 +0900

    init : package 생성

--- ros/catkin_ws/build/ssafy_msgs/ament_cmake_uninstall_target/ament_cmake_uninstall_target.cmake
+++ ros/catkin_ws/build/ssafy_msgs/ament_cmake_uninstall_target/ament_cmake_uninstall_target.cmake
@@ -2,7 +2,7 @@
 # ament_cmake_core/cmake/uninstall_target/ament_cmake_uninstall_target.cmake.in
 
 function(ament_cmake_uninstall_target_remove_empty_directories path)
-  set(install_space "C:/Users/SSAFY/Desktop/tt/catkin_ws/catkin_ws/install/ssafy_msgs")
+  set(install_space "C:/Users/SSAFY/Desktop/project/S09P22B201/ros/catkin_ws/install/ssafy_msgs")
   if(install_space STREQUAL "")
     message(FATAL_ERROR "The CMAKE_INSTALL_PREFIX variable must not be empty")
   endif()
@@ -29,7 +29,7 @@ function(ament_cmake_uninstall_target_remove_empty_directories path)
 endfunction()
 
 # uninstall files installed using the standard install() function
-set(install_manifest "C:/Users/SSAFY/Desktop/tt/catkin_ws/catkin_ws/build/ssafy_msgs/install_manifest.txt")
+set(install_manifest "C:/Users/SSAFY/Desktop/project/S09P22B201/ros/catkin_ws/build/ssafy_msgs/install_manifest.txt")
 if(NOT EXISTS "${install_manifest}")
   message(FATAL_ERROR "Cannot find install manifest: ${install_manifest}")
 endif()
--- ros/catkin_ws/build/sub1/prefix_override/sitecustomize.py
+++ ros/catkin_ws/build/sub1/prefix_override/sitecustomize.py
@@ -1,3 +1,3 @@
 import sys
 sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = 'C:\\Users\\SSAFY\\Desktop\\tt\\catkin_ws\\catkin_ws\\install\\sub1'
+sys.prefix = sys.exec_prefix = 'C:\\Users\\SSAFY\\Desktop\\project\\S09P22B201\\ros\\catkin_ws\\install\\sub1'
